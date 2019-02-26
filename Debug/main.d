40e7fb69f631d6d347bc13efbd09ec129f2b3dd3
f.guerra <pwar66@gmail.com>
2019-02-26 10:07:23 -0800

    Init commit

---
+++ Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/temp/ccsv8/ccs_base/msp430/include/msp430.h
+main.obj: D:/temp/ccsv8/ccs_base/msp430/include/msp430fr5969.h
+main.obj: D:/temp/ccsv8/ccs_base/msp430/include/in430.h
+main.obj: D:/temp/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h
+main.obj: D:/temp/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+D:/temp/ccsv8/ccs_base/msp430/include/msp430.h: 
+D:/temp/ccsv8/ccs_base/msp430/include/msp430fr5969.h: 
+D:/temp/ccsv8/ccs_base/msp430/include/in430.h: 
+D:/temp/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h: 
+D:/temp/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h: 
